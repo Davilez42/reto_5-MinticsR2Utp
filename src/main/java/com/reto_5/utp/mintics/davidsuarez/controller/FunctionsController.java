7035ec4231bfaf5c07efe049a2b59a56b1b71569
Jose David Suarez Cardona <josedavid4227@gmail.com>
2022-08-11 13:33:44 -0500

    Eliminar antigua gui

--- src/main/java/com/reto_5/utp/mintics/davidsuarez/controller/FunctionsController.java
+++
@@ -1,9 +0,0 @@
-package com.reto_5.utp.mintics.davidsuarez.controller;
-
-import java.util.List;
-
-public interface FunctionsController {
-        public void getReportLideres();
-        public void getReportProyects();
-        public void getReportBuys();
-}
